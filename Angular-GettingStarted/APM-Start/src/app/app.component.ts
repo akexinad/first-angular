faaf6ce9d5f3a23d59829970418f77f5e4d0b301
Ceccattini <Danny.Ceccattini@lendlease.com>
2019-05-31 13:43:47 +1000

    firs commit

---
+++ Angular-GettingStarted/APM-Start/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from "@angular/core";
+
+// The decorator is where all your metadata goes.
+// NOTE: Decorator is a function, so you begin with parentheses.
+@Component({
+  // The selector gives the component a position in the structure. This component is the root component.
+  selector: 'pm-root',
+  // The template value will tell is what is rendered from this component.
+  template: `
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <pm-products></pm-products>
+    </div>
+  `
+})
+
+export class AppComponent {
+  pageTitle: string = "Monty Python Lolly Shop"
+}
---
+++ Angular-GettingStarted/APM-Start/src/app/products/product_list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { componentFactoryName } from "@angular/compiler";
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product_list.component.html',
+})
+
+export class ProductListComponent {
+    
+}
