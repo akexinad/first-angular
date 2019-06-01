78cc2f5d8b2879fccfbd63d2fdd1e1501263082c
akexinad <danixeka@gmail.com>
2019-06-01 10:57:03 +1000

    added comments

--- Angular-GettingStarted/APM-Start/src/app/app.module.ts
+++ Angular-GettingStarted/APM-Start/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// formsModule needs to be imported in order to use ngModel.
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
