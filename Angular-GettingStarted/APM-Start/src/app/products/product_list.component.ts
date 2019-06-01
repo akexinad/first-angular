59e4ec0ee94f23267214facebf53f97d08a3f5de
akexinad <danixeka@gmail.com>
2019-06-01 16:15:31 +1000

    passing data to a nested component using @input

--- Angular-GettingStarted/APM-Start/src/app/products/product_list.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product_list.component.ts
@@ -80,7 +80,7 @@ export class ProductListComponent implements OnInit{
 
     constructor() {
       this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+      this.listFilter = null;
     }
 
     performFilter(filterBy: string): IProduct[] {
