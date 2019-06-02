ef2805d691db7b42a3409beb48ac47a474a699a9
akexinad <danixeka@gmail.com>
2019-06-02 13:43:23 +1000

    passing parameters to a route

--- Angular-GettingStarted/APM-Start/src/app/products/product-detail.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 import IProduct from "./productInterface";
 
@@ -8,10 +9,23 @@ import IProduct from "./productInterface";
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
+  pageTitle: string = 'Product Detail';
+  product: IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${ id }`;
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "assets/images/leaf_rake.jpg"
+    }
   }
-
 }
