80b9df17f37de738ef5ea94ebb392883ef433af9
akexinad <danixeka@gmail.com>
2019-06-02 13:49:13 +1000

    activating a route with code

--- Angular-GettingStarted/APM-Start/src/app/products/product-detail.component.ts
+++ Angular-GettingStarted/APM-Start/src/app/products/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 
 import IProduct from "./productInterface";
 
@@ -12,7 +12,8 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     let id = +this.route.snapshot.paramMap.get('id');
@@ -27,5 +28,9 @@ export class ProductDetailComponent implements OnInit {
       "starRating": 3.2,
       "imageUrl": "assets/images/leaf_rake.jpg"
     }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 }
